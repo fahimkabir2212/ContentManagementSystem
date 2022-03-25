072ba8cc3a800812b2137123d0d1edad0aaae579
Emokid <Emokid@TEAMOS-PC>
2022-03-25 23:20:05 +0600

    Add project files.

---
+++ ContentManagementSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ContentManagementSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
